d951451c5f4b48faefd57cb0b1c01cc009e9b1ac
Praveen20010317 <praveen18cs100@gmail.com>
2022-02-04 09:10:57 +0000

    initail commit

---
+++ reactapp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  var a = 10;
+  return (
+    <div className="hello">
+      <input type="text" placeholder="Enter Name"/>
+      <input type="button" value="Submit" />
+      <h1>Hello World{a}</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ reactapp/src/Hello.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function Hello() {
+  var a = 10;
+  return (
+    <div className="page">
+      <h1>Welcome to Hello Page {a}</h1>
+    </div>
+  );
+}
+
+export default Hello;
