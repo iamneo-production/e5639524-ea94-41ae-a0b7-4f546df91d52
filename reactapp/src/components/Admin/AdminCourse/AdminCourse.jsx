684aaef579c513912d2ad2184b68ccca97c0bb44
Shruti <contactshrutisi@gmail.com>
2022-03-06 12:00:28 +0530

    Admin Course Updates

--- reactapp/src/components/Admin/AdminCourse/AdminCourse.jsx
+++ reactapp/src/components/Admin/AdminCourse/AdminCourse.jsx
@@ -11,6 +11,7 @@ import './AdminCourse.css';
 const fetchData = async () => {
     const res = await fetch('http://localhost:8082/course/list');
    
+   
     console.log(res);
     
     const json = await res.json();
@@ -27,12 +28,7 @@ function AdminCourse(props) {
         setCourseList(courseList)
     })
   }, []);
-  const refreshData= ()=> {  //func for fc
-    fetchData().then(courseList => {
-        setCourseList(courseList); 
-    });
 
-     };
      const getList = (props, list) => {
         let result = [];
         let rowCount = 1;
@@ -41,21 +37,31 @@ function AdminCourse(props) {
         if(!list) return ;
         list.forEach(element => {
             result.push(<div class="course-info">
-                <div class="course-image"><img class="image" src={"/images/"+element.id+".jpg"} /></div>
+                {/* <div class="course-image"><img class="image" src={"/images/"+element.id+".jpg"} /></div> */}
+                <div class = "left-section"> 
                 <div class="course-name">{element.name}</div> 
-                <div class="course-location">{element.location}</div>
+                {/* <div class="course-location">{element.location} </div> */}
                 <div class = "course-duration">{element.duration}</div>
+                <div class = "course-available-timings">{element.cst}</div>
+                </div>
+                <div class = "right-section"> 
+                <div class = "number-of-students">Number of Students: {element.nos}</div>
+                <div class = "course-description">{element.description}</div>
+                <div class = "buttons">
+                    <img class="edit button" src={"/icon/edit.jpg"} />
+                    <img class="delete button" src={"/icon/delete.jpg"} />
+                </div>
+                </div>
                 </div>);
         });
         return result;
     };    
     return (
         <div> 
-            <div class = "SearchBar">Search: <input type = "text" name = "keyword" placeholder="Search courses"/> </div>
+            <div class = "SearchBar"> <input type = "text" name = "keyword" placeholder="Type here to search course"/>
+             <button class = "Search-button"> Search </button></div>
             { getList(props, courseList) }
         </div>
     );
 }
-
-
 export default AdminCourse;
