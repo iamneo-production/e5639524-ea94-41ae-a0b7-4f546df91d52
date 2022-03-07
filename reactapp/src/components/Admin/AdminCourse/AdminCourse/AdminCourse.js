ed62a1e9171a4fc0c77725e6ff0b85a0d873c55b
Shruti <contactshrutisi@gmail.com>
2022-03-07 14:32:12 +0530

    Admin Course

--- reactapp/src/components/Admin/AdminCourse/AdminCourse/AdminCourse.js
+++ reactapp/src/components/Admin/AdminCourse/AdminCourse/AdminCourse.js
@@ -1,33 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import editLogo from '../../../icon/edit.jpg'
-import deleteLogo from '../../../icon/delete.jpg'
-import UserService from '../../../service/UserService';
-
+import React from 'react';
+import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 
 import './AdminCourse.css';
-
+// import './UserService.js';
 
 const fetchData = async () => {
     const res = await fetch('http://localhost:8082/course/list');
-   
-   
     console.log(res);
-    
     const json = await res.json();
     console.log(json);
     return json;
 }
-
+// const fetchDeleteData = async (id) => {
+//     const res = await fetch('http://localhost:8082/course/list'+id,{method:'DELETE'});
+//     console.log(res);
+//     const json = await res.json();
+//     console.log(json);
+//     return json;
+// }
 
 function AdminCourse(props) {
-    const [courseList, setCourseList] = useState([])
+    let navigate = useNavigate();
+    let [courseList, setCourseList, search, setSearch] = useState([]);
 
   useEffect(() => {
     fetchData().then(courseList => {
         setCourseList(courseList)
     })
   }, []);
+  const changeSearchHandler = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const editCourse = (id) => {
+    navigate(`updateCourse/${id}`);
+  };
+
+//   const deleteCourse = (id) => {
+//     // DeleteService.deleteCourse(id).then((res) => {
+//     //   setCourseList(courseList.filter((course)=> course.courseId !== id))
+// //  }
+//   };
 
      const getList = (props, list) => {
         let result = [];
@@ -37,10 +52,8 @@ function AdminCourse(props) {
         if(!list) return ;
         list.forEach(element => {
             result.push(<div class="course-info">
-                {/* <div class="course-image"><img class="image" src={"/images/"+element.id+".jpg"} /></div> */}
                 <div class = "left-section"> 
-                <div class="course-name">{element.name}</div> 
-                {/* <div class="course-location">{element.location} </div> */}
+                <div class="course-name">{element.name}</div>
                 <div class = "course-duration">{element.duration}</div>
                 <div class = "course-available-timings">{element.cst}</div>
                 </div>
@@ -48,8 +61,11 @@ function AdminCourse(props) {
                 <div class = "number-of-students">Number of Students: {element.nos}</div>
                 <div class = "course-description">{element.description}</div>
                 <div class = "buttons">
-                    <img class="edit button" src={"/icon/edit.jpg"} />
-                    <img class="delete button" src={"/icon/delete.jpg"} />
+                    <img class="edit button" src={"/icon/edit.jpg"} onClick="editCourse({element.id})" />
+                    <img class="delete button" src={"/icon/delete.jpg"} onClick="deleteCourse({element.id})" />
+                    {/* { isAdmin && (
+                     <button class = "buttons" onClick="fetchDeleteData({element.id})"> Delete Course</button>
+                )} */}
                 </div>
                 </div>
                 </div>);
--- springapp/src/main/java/com/artsandcrafts/controller/CourseController.java
+++ springapp/src/main/java/com/artsandcrafts/controller/CourseController.java
@@ -4,6 +4,8 @@ package com.artsandcrafts.controller;
 
 import com.artsandcrafts.model.Course;
 import com.artsandcrafts.service.CourseService;
+import com.example.internship.model.Course;
+import com.example.internship.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,23 +14,22 @@ import org.springframework.http.HttpStatus;
 import java.util.List;
 
 @RestController
-@RequestMapping("/admin")
-@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/course") //url
+@CrossOrigin //security purposes
 public class CourseController {
     @Autowired
     private CourseService courseService;
 
-    @PostMapping("/addCourse")
-	public String add(@RequestBody AddCourse addCourse) {
-		addCourseService.saveAddCourse(addCourse);
-		return "New Course Added";
-	}
 
-    @GetMapping("/viewCourse")
+
+    @GetMapping("/list") //url
     public List<Course> list(){
         return courseService.getAll();
     }
 
-
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
 }
 
